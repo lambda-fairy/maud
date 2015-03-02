74475f922bd149f88ef849a5187d88426fdb8b5a
Chris Wong <lambda.fairy@gmail.com>
2015-03-01 20:07:50 -0500

    Update Rust

--- maud_macros/src/lib.rs
+++ maud_macros/src/lib.rs
@@ -8,14 +8,14 @@ extern crate maud;
 
 use syntax::ast::TokenTree;
 use syntax::codemap::Span;
-use syntax::ext::base::{ExtCtxt, MacExpr, MacResult};
+use syntax::ext::base::{ExtCtxt, MacEager, MacResult};
 use rustc::plugin::Registry;
 
 mod parse;
 mod render;
 
 fn expand_html<'cx>(cx: &'cx mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'cx> {
-    MacExpr::new(parse::parse(cx, args, sp))
+    MacEager::expr(parse::parse(cx, args, sp))
 }
 
 #[plugin_registrar]
