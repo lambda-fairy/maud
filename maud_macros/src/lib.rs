e5711929e338fa5fc5c628711c0ee10c5317c1d8
Chris Wong <lambda.fairy@gmail.com>
2015-04-11 10:19:28 +1200

    print-expansion for all your expansion printing needs

--- maud_macros/src/lib.rs
+++ maud_macros/src/lib.rs
@@ -10,13 +10,18 @@ extern crate maud;
 use syntax::ast::TokenTree;
 use syntax::codemap::Span;
 use syntax::ext::base::{ExtCtxt, MacEager, MacResult};
+use syntax::print::pprust;
 use rustc::plugin::Registry;
 
 mod parse;
 mod render;
 
 fn expand_html<'cx>(cx: &'cx mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'cx> {
-    MacEager::expr(parse::parse(cx, args, sp))
+    let expr = parse::parse(cx, args, sp);
+    if cfg!(feature = "print-expansion") {
+        println!("{}", pprust::expr_to_string(&expr));
+    }
+    MacEager::expr(expr)
 }
 
 #[plugin_registrar]
