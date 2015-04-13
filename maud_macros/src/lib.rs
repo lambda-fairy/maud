4653b2e2d88f4f234753ee1addeef4e8b53b9569
Chris Wong <lambda.fairy@gmail.com>
2015-04-13 20:20:02 +1200

    Add html_debug! macro; remove print-expansion feature

--- maud_macros/src/lib.rs
+++ maud_macros/src/lib.rs
@@ -16,15 +16,26 @@ use rustc::plugin::Registry;
 mod parse;
 mod render;
 
-fn expand_html<'cx>(cx: &'cx mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'cx> {
+fn expand_html_common<'cx>(cx: &'cx mut ExtCtxt, sp: Span, args: &[TokenTree],
+                           debug: bool) -> Box<MacResult + 'cx> {
     let expr = parse::parse(cx, args, sp);
-    if cfg!(feature = "print-expansion") {
-        println!("{}", pprust::expr_to_string(&expr));
+    if debug {
+        cx.span_note(sp, &format!("expansion:\n{}",
+                                  pprust::expr_to_string(&expr)));
     }
     MacEager::expr(expr)
+}
+
+fn expand_html<'cx>(cx: &'cx mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'cx> {
+    expand_html_common(cx, sp, args, false)
+}
+
+fn expand_html_debug<'cx>(cx: &'cx mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'cx> {
+    expand_html_common(cx, sp, args, true)
 }
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
     reg.register_macro("html", expand_html);
+    reg.register_macro("html_debug", expand_html_debug);
 }
