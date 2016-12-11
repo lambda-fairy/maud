5410e73e11ff2bb9ad4a18a5e147d5bc7fe2055d
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-12-11 22:14:10 +0200

    Add meta title element

--- maud_extras/lib.rs
+++ maud_extras/lib.rs
@@ -82,3 +82,29 @@ impl<T: AsRef<str>, U: AsRef<str>> Render for Meta<T, U> {
         }
     }
 }
+
+/// Generate <title> element.
+///
+/// # Example
+///
+/// ```rust
+/// # #![feature(plugin)]
+/// # #![plugin(maud_macros)]
+/// # extern crate maud;
+/// # extern crate maud_extras;
+/// # use maud_extras::*;
+/// # fn main() {
+/// let markup = html! { (Title("Maud")) };
+/// assert_eq!(markup.into_string(),
+///            r#"<title>Maud</title>"#);
+/// # }
+/// ```
+pub struct Title<T: AsRef<str>>(pub T);
+
+impl<T: AsRef<str>> Render for Title<T> {
+    fn render(&self) -> Markup {
+        html! {
+            title (self.0.as_ref())
+        }
+    }
+}
