f21d1b578e42207abdb3ae55bba61147521d110c
Chris Wong <lambda.fairy@gmail.com>
2017-01-15 15:37:13 +1300

    Update to rustc 1.16.0-nightly (1a2ed98d3 2017-01-13)

--- maud_macros/src/lints/doctype_html.rs
+++ maud_macros/src/lints/doctype_html.rs
@@ -30,7 +30,7 @@ impl<'a, 'tcx> LateLintPass<'a, 'tcx> for DoctypeHtml {
             if s.as_str().eq_ignore_ascii_case("<!doctype html>");
             // ... and the callee is `maud::PreEscaped`
             if let ExprPath(ref qpath) = path_expr.node;
-            let def_id = cx.tcx.tables().qpath_def(qpath, path_expr.id).def_id();
+            let def_id = cx.tables.qpath_def(qpath, path_expr.id).def_id();
             if match_def_path(cx, def_id, &["maud", "PreEscaped", "{{constructor}}"]);
             then {
                 cx.struct_span_lint(MAUD_DOCTYPE_HTML, expr.span,
