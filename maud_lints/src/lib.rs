540226f1c4db4e7ae54986e297c7ee2425fe12c0
Chris Wong <lambda.fairy@gmail.com>
2018-08-26 20:27:39 +1200

    Remove `#![feature(macro_vis_matcher)]`

--- maud_lints/src/lib.rs
+++ maud_lints/src/lib.rs
@@ -1,7 +1,6 @@
 #![feature(plugin_registrar)]
 #![feature(rustc_private)]
 #![feature(macro_at_most_once_rep)]
-#![feature(macro_vis_matcher)]
 #![recursion_limit = "1000"]  // if_chain
 
 #![doc(html_root_url = "https://docs.rs/maud_lints/0.18.1")]
