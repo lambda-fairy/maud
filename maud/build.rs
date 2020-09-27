4a06b214466029eb5c046cd682a2454b219d4113
Chris Wong <lambda.fairy@gmail.com>
2020-09-28 00:13:10 +1000

    Add missing `.unwrap()`

--- maud/build.rs
+++ maud/build.rs
@@ -1,7 +1,7 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    match version_meta().channel {
+    match version_meta().unwrap().channel {
         Channel::Dev | Channel::Nightly => println!("cargo:rustc-cfg=unstable"),
         Channel::Beta | Channel::Stable => {}
     }
