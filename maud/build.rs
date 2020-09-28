2ab653ead72d2959c4dc12a072b77cbc175d8916
Chris Wong <lambda.fairy@gmail.com>
2020-09-28 10:51:41 +1000

    Default to stable if rustc version fails to parse

--- maud/build.rs
+++ maud/build.rs
@@ -1,7 +1,7 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    match version_meta().unwrap().channel {
+    match version_meta().map(|v| v.channel).unwrap_or(Channel::Stable) {
         Channel::Dev | Channel::Nightly => println!("cargo:rustc-cfg=unstable"),
         Channel::Beta | Channel::Stable => {}
     }
