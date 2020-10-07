b34d7aa93834227b7b862d2422aa13bfe2f747a4
Rob Ede <robjtede@icloud.com>
2020-10-07 23:54:44 +0100

    Add actix-web example

---
+++ maud/examples/actix.rs
@@ -0,0 +1,25 @@
+// do not use this line in your application
+use actix_web_dep as actix_web;
+
+use actix_web::{get, App, HttpServer, Result as AwResult};
+use maud::{html, Markup};
+use std::io;
+
+#[get("/")]
+async fn index() -> AwResult<Markup> {
+    Ok(html! {
+        html {
+            body {
+                h1 { "Hello World!" }
+            }
+        }
+    })
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
