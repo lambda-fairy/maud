8990049101246aec0cf3c784182ca819c2c54ebc
Chris Wong <lambda.fairy@gmail.com>
2021-03-21 22:20:37 +1100

    Improve error message for non-string literals (#265)
    Closes #208

--- maud/tests/warnings/non-string-literal.rs
+++ maud/tests/warnings/non-string-literal.rs
@@ -3,5 +3,14 @@ use maud::html;
 fn main() {
     html! {
         42
+        42usize
+        42.0
+        'a'
+        b"a"
+        b'a'
+
+        // `true` and `false` are only considered literals in attribute values
+        input disabled=true;
+        input disabled=false;
     };
 }
