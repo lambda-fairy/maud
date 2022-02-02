579fa7340450739e162ea72754a649e8d6ee2afb
Rob Ede <robjtede@icloud.com>
2022-02-02 13:58:02 +0000

    Experimental CI update

--- maud/tests/errors.rs
+++ maud/tests/errors.rs
@@ -1,7 +1,6 @@
-use trybuild::TestCases;
-
+#[rustversion::stable(1.45.2)] // MSRV
 #[test]
 fn run_warnings() {
-    let config = TestCases::new();
+    let config = trybuild::TestCases::new();
     config.compile_fail("tests/warnings/*.rs");
 }
