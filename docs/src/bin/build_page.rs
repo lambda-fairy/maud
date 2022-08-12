54483afa6dff578921d02ac4fe5e07674f50170d
Chris Wong <lambda.fairy@gmail.com>
2022-08-12 23:25:36 +1000

    Fix Clippy

--- docs/src/bin/build_page.rs
+++ docs/src/bin/build_page.rs
@@ -149,9 +149,10 @@ fn highlight_code<'a>(root: &'a AstNode<'a>) -> Result<(), Box<dyn Error>> {
                 literal.push('\n');
             }
             let html = highlighted_html_for_string(&literal, &ss, syntax, &theme)?;
-            let mut html_block = NodeHtmlBlock::default();
-            html_block.literal = html.into_bytes();
-            data.value = NodeValue::HtmlBlock(html_block);
+            data.value = NodeValue::HtmlBlock(NodeHtmlBlock {
+                literal: html.into_bytes(),
+                ..Default::default()
+            });
         }
     }
     Ok(())
