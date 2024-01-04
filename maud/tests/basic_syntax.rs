ab669657a700ed80f5fc2ba35a04b4d76cf6ecbf
Chris Wong <lambda.fairy@gmail.com>
2024-01-04 15:25:39 +1100

    Replace "byte_string" with "raw_string" in test

--- maud/tests/basic_syntax.rs
+++ maud/tests/basic_syntax.rs
@@ -201,10 +201,10 @@ fn raw_string_literals_in_attribute_names() {
 
 #[test]
 fn other_literals_in_attribute_names() {
-    let result = html! { this r#"byte_string"#="false" 123="123" 123usize "2.5" true of-course {} };
+    let result = html! { this r#"raw_string"#="false" 123="123" 123usize "2.5" true of-course {} };
     assert_eq!(
         result.into_string(),
-        r#"<this byte_string="false" 123="123" 123usize 2.5 true of-course></this>"#
+        r#"<this raw_string="false" 123="123" 123usize 2.5 true of-course></this>"#
     );
 }
 
