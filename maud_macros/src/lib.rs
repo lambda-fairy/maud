78080020b61b359656f10a29bfd9691083b46e18
Vidhan Bhatt <me@vidhan.io>
2024-01-08 02:01:16 +0530

    feat: modernize parser

--- maud_macros/src/lib.rs
+++ maud_macros/src/lib.rs
@@ -8,30 +8,29 @@ extern crate proc_macro;
 mod ast;
 mod escape;
 mod generate;
-mod parse;
 
-use proc_macro2::{Ident, Span, TokenStream, TokenTree};
-use proc_macro_error::proc_macro_error;
+use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro]
-#[proc_macro_error]
 pub fn html(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    expand(input.into()).into()
-}
-
-fn expand(input: TokenStream) -> TokenStream {
-    let output_ident = TokenTree::Ident(Ident::new("__maud_output", Span::mixed_site()));
     // Heuristic: the size of the resulting markup tends to correlate with the
     // code size of the template itself
     let size_hint = input.to_string().len();
-    let markups = parse::parse(input);
+    let markups = parse_macro_input!(input as ast::Markups);
+
+    expand(size_hint, markups).into()
+}
+
+fn expand(size_hint: usize, markups: ast::Markups) -> TokenStream {
+    let output_ident = Ident::new("__maud_output", Span::mixed_site());
     let stmts = generate::generate(markups, output_ident.clone());
-    quote!({
+    quote! {{
         extern crate alloc;
         extern crate maud;
         let mut #output_ident = alloc::string::String::with_capacity(#size_hint);
         #stmts
         maud::PreEscaped(#output_ident)
-    })
+    }}
 }
